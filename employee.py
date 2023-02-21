ff4158226c95f868223a1a722a4a53d7f33cd37d
Debabrata <debabrataghosh7585@gmail.com>
2023-02-21 14:21:55 +0530

    built the searchbar in the employee page

--- employee.py
+++ employee.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from PIL import Image,ImageTk
+from tkinter import ttk
 
 class employeeClass:
     def __init__(self, root):
@@ -7,7 +8,22 @@ class employeeClass:
         self.root.geometry("1100x500+200+130")
         self.root.title("Inventory Management System | by Debabrata Ghosh")
         self.root.config(bg="white")
+        self.root.focus_force()
 
+        ##---Search Frame---##
+        SearchFrame=LabelFrame(self.root, text="Search Employee",font=("goudy old style",12,"bold"),bd=2,relief=RIDGE, bg="white")
+        SearchFrame.place(x=250, width=600, height=70)
+
+        #---options--##
+        cmb_search=ttk.Combobox(SearchFrame,values=("Select", "Email", "Name", "Contact"), state="readonly", justify=CENTER, font=("goudy old style",15))
+        cmb_search.place(x=10,y=10,width=180)
+        cmb_search.current(0)
+
+        txt_search=Entry(SearchFrame,font=("goudy old style",15),bg="lightyellow").place(x=200,y=10)
+        btn_search=Button(SearchFrame,text="Search", font=("goudy old style",15),bg="#4caf50", fg="white", cursor="hand2").place(x=410,y=9, width=150, height=30)
+
+        ##---Title---##
+        ##Yet to Start
 
 if __name__=="__main__":
     root=Tk()
